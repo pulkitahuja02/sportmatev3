05302a882d59b83bb94b2fa286715e199d372c83
Pulkit <ahujapulkit0202@gmail.com>
2025-02-22 02:22:28 +0530

    First commit to just test if taiwind css is loading or not

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => 
+{
+  return(
+    <div>
+       <div className="container bg-red-500">
+           <h1>Hello tailwind , pulkit this side</h1>
+       </div>
+    </div>
+  )
+}
+
+export default App
